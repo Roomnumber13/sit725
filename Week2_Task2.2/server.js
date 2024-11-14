7a215c5f9f103f170b0751bcf5a1c8d8cd577da8
Roomnumber13 <rajkumar.rajendran197@gmail.com>
2024-11-14 11:35:58 +1100

    Update file

--- Week2_Task2.2/server.js
+++ Week2_Task2.2/server.js
@@ -4,7 +4,9 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 var port = process.env.port || 3000;
-
+const addTwoNumber=(n1,n2)=>{
+    return n1+n2;
+}
 app.get("/addTwoNumber", (req,res)=>{
     const n1= parseInt(req.query.n1);
     const n2=parseInt(req.query.n2);
