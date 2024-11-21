edf7c854b4949a501a1d1d91301dac203ccf745e
Roomnumber13 <rajkumar.rajendran197@gmail.com>
2024-11-21 14:05:40 +1100

    Update file

--- Week3_Task3.3/public/js/extra.js
+++ Week3_Task3.3/public/js/extra.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.modal').modal();
   });
+  $(document).ready(function(){
+    $('.waves-effect waves-light btn').on('click', function(){
+      $('waves-effect waves-light btn').css('background','#ccc');
+    });
+  });
