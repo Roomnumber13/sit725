a073c14b5c59acde233a5e193bdb6cf60007ebb2
Roomnumber13 <rajkumar.rajendran197@gmail.com>
2024-11-28 10:32:33 +1100

    Update file

--- Week4_Task4.1/mongo_db.js
+++ Week4_Task4.1/mongo_db.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:3000/mydb";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
