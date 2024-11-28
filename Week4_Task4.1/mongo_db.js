9eeec3392d00349d94121fcac4f8ea4f062b2ab3
Roomnumber13 <rajkumar.rajendran197@gmail.com>
2024-11-28 11:14:19 +1100

    Update file

--- Week4_Task4.1/mongo_db.js
+++ Week4_Task4.1/mongo_db.js
@@ -3,6 +3,10 @@ var url = "mongodb+srv://mike:229049@cluster0.qr9ei.mongodb.net/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
+  var dbo = db.db("mydb");
+  dbo.createCollection("customers", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
 });
